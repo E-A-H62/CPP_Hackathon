54febb551f0dad90a95a2641e18e668d8e094cb2
E-A-H62 <eahernandez1212@gmail.com>
2025-04-09 18:38:10 -0700

    Added basic login page to frontend

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RestaurantRecommender from './RestaurantRecommender';
 import RestaurantPage from './RestaurantPage';
+import Login from './Login';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<RestaurantRecommender/>} />
+          <Route path="/" element={<Login />} />
+          <Route path="/recommender" element={<RestaurantRecommender/>} />
           <Route path="/restaurant" element={<RestaurantPage/>} />
         </Routes>
       </div>
